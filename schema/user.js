12625fa3ea171e7f1ab857744889f7912dca86f3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-01-13 11:31:59 +0100

    user schema

---
+++ schema/user.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+// Create a User model
+export const User = mongoose.model("User", userSchema);
