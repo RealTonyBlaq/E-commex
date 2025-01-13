00f78274af6e83d5e769cc773ca43bdd4ffb8303
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-01-13 11:27:47 +0100

    update

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 /* App server */
 import express from 'express';
+import router from './routes/index.js';
+import connectDB from './utils/db.js';
+import { config } from 'dotenv';
 
+config();
+connectDB();
 
+const PORT = process.env.PORT || 3000;
 const app = express();
-const PORT = 3000;
-
+app.use(express.json());
+app.use(router);
 
 app.get('/', (req, res) => {
     return res.send('Hello World\n').status(200);
 });
 
-
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Server connected and listening on http://127.0.0.1:${PORT}`);
 });
