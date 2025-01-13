e875e020f4a646eb9cd0f0cb1f5a175e15d66cbf
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-01-13 11:30:23 +0100

    category schema

---
+++ schema/category.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const CategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export const Category = mongoose.model("Category", CategorySchema);
