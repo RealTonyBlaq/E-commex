4912e554d21d47eaeb607513416d671b0d1d203f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-01-19 11:16:17 +0100

    fix

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 /* App server */
-import express from 'express';
-import router from './routes/index.js';
-import connectDB from './utils/db.js';
-import { config } from 'dotenv';
+import cors from "cors";
+import express from "express";
+import router from "./routes/index.js";
+import connectDB from "./utils/db.js";
+import { config } from "dotenv";
+import process from "process";
+import { StatusCodes } from "http-status-codes";
 
 config();
 connectDB();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
 app.use(express.json());
 app.use(router);
+app.use(cors());
 
-app.get('/', (req, res) => {
-    return res.send('Hello World\n').status(200);
+app.get("/", (req, res) => {
+  return res.send("Hello World\n").status(StatusCodes.OK);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server connected and listening on http://127.0.0.1:${PORT}`);
+  console.log(`Server connected and listening on http://127.0.0.1:${PORT}`);
 });
