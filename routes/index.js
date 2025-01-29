d0307d7c8a4b951e7667a96acf63eb5ee1c12958
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-01-29 22:55:50 +0100

    added /users POST GET PUT DELETE

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-import { Router } from 'express';
-import UserController from '../controllers/UserController.js';
+import { Router } from "express";
+import UserController from "../controllers/UserController.js";
 
 const router = Router();
 
-router.post('/users', (req, res) => UserController.createUser(req, res));
-
+router
+  .route("/users")
+  .post(UserController.createUser)
+  .get(UserController.getUser)
+  .put(UserController.updateUser)
+  .delete(UserController.deleteUser);
 
 export default router;
