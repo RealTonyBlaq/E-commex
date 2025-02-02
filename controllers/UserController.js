511c60be08d9e9da23c518554bc7f112989b2121
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-02-02 21:52:33 +0100

    update

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,10 @@
-import { User } from "../schema/user.js";
-import bcrypt from "bcryptjs";
-import { isValidObjectId } from "mongoose";
-import { StatusCodes } from "http-status-codes";
+import { User } from '../schema/user.js';
+import bcrypt from 'bcryptjs';
+import { isValidObjectId } from 'mongoose';
+import { StatusCodes } from 'http-status-codes';
 
 class UserController {
-  static async createUser(req, res) {
+  static async createUser (req, res) {
     try {
       const { firstName, lastName, email, password, phoneNumber } = req.body;
 
@@ -12,7 +12,7 @@ class UserController {
       if (existingUser) {
         return res
           .status(StatusCodes.BAD_REQUEST)
-          .json({ error: "User already exists" });
+          .json({ error: 'User already exists' });
       }
 
       const hashedPassword = await bcrypt.hash(password, 10);
@@ -22,7 +22,7 @@ class UserController {
         lastName,
         email,
         password: hashedPassword,
-        phoneNumber,
+        phoneNumber
       });
       await user.save();
       return res.status(StatusCodes.CREATED).json(user);
@@ -31,16 +31,20 @@ class UserController {
     }
   }
 
-  static async getUser(req, res) {
-    const { id } = req.params;
-    if (!id) return res.status(StatusCodes.OK).json(await User.find());
+  static async getUser (req, res) {
+    const id = req.query.id;
+    if (!id) {
+      const allUsers = await User.find();
+      return res.status(StatusCodes.OK).json(allUsers);
+    }
+    if (!isValidObjectId(id)) return res.status(StatusCodes.NOT_FOUND).json({ error: 'Invalid ID' });
 
     try {
       const user = await User.findById(id);
       if (!user) {
         return res
           .status(StatusCodes.NOT_FOUND)
-          .json({ error: "User not found" });
+          .json({ error: 'User not found' });
       }
       return res.status(StatusCodes.OK).json(user);
     } catch (error) {
@@ -48,10 +52,13 @@ class UserController {
     }
   }
 
-  static async updateUser(req, res) {
-    const { id } = req.params;
-    const allowedUpdates = ["firstName", "lastName", "password", "phoneNumber"];
+  static async updateUser (req, res) {
+    const id = req.query.id;
+    const allowedUpdates = ['firstName', 'lastName', 'password', 'phoneNumber'];
     const updates = {};
+
+    if (!id) return res.status(StatusCodes.BAD_REQUEST).json({ error: 'No ID passed' });
+    if (!isValidObjectId(id)) return res.status(StatusCodes.NOT_FOUND).json({ error: 'Invalid ID' });
 
     Object.keys(req.body).forEach((update) => {
       if (allowedUpdates.includes(update)) {
@@ -66,12 +73,12 @@ class UserController {
     try {
       const user = await User.findByIdAndUpdate(id, updates, {
         new: true,
-        runValidators: true,
+        runValidators: true
       });
       if (!user) {
         return res
           .status(StatusCodes.NOT_FOUND)
-          .json({ error: "User not found" });
+          .json({ error: 'User not found' });
       }
       return res.status(StatusCodes.OK).json(user);
     } catch (error) {
@@ -79,14 +86,11 @@ class UserController {
     }
   }
 
-  static async deleteUser(req, res) {
-    const { id } = req.params;
+  static async deleteUser (req, res) {
+    const id = req.query.id;
 
-    if (!isValidObjectId(id)) {
-      return res
-        .status(StatusCodes.NOT_FOUND)
-        .json({ error: "User not found" });
-    }
+    if (!id) return res.status(StatusCodes.BAD_REQUEST).json({ error: 'No ID passed' });
+    if (!isValidObjectId(id)) return res.status(StatusCodes.NOT_FOUND).json({ error: 'Invalid ID' });
 
     try {
       await User.findByIdAndDelete(id);
