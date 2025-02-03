0bb99924526d9e2a40a9af9bed5eb11d31ae9afc
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-02-03 19:10:11 +0100

    Product controller

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,33 +1,53 @@
-import { isValidObjectId } from "mongoose";
-import { Product } from "../schema/product";
-import { StatusCodes } from "http-status-codes";
+import { isValidObjectId } from 'mongoose';
+import { Product } from '../schema/product';
+import { StatusCodes } from 'http-status-codes';
 
 class ProductController {
-    static async createProduct(req, res) {
-        const { name, description, price, stockQuantity, categoryId,  } = req.body;
-        if (!name || !description || !price || !stockQuantity || !categoryId) {
-            return res
-                .status(StatusCodes.BAD_REQUEST)
-                .json({ error: "All fields are required" });
-        }
-        if (!isValidObjectId(categoryId)) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ error: "Invalid category ID" });
-        }
-        try {
-            const product = new Product({
-                name,
-                description,
-                price,
-                stockQuantity,
-                categoryId,
-            });
-            await product.save();
-            return res.status(StatusCodes.CREATED).json(product);
-        } catch (error) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
-        }
+  static async createProduct (req, res) {
+    const { name, description, price, stockQuantity, categoryId, imageURL } = req.body;
+    if (!name || !description || !price || !stockQuantity || !categoryId || !imageURL) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: 'All fields are required' });
     }
-}
+    if (!isValidObjectId(categoryId)) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Invalid category ID' });
+    }
+    try {
+      const product = new Product({
+        name,
+        description,
+        price,
+        stockQuantity,
+        categoryId,
+        imageURL
+      });
+      await product.save();
+      return res.status(StatusCodes.CREATED).json(product);
+    } catch (error) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
+    }
+  }
+  static async getProduct (req, res) {
+    const id = req.query.id;
+    if (!id) {
+      const allProducts = await Product.find();
+      return res.status(StatusCodes.OK).json(allProducts);
+    }
+    if (!isValidObjectId(id)) return res.status(StatusCodes.NOT_FOUND).json({ error: 'Invalid Product ID' });
 
+    try {
+      const product = await Product.findById(id);
+      if (!product) {
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ error: 'Product not found' });
+      }
+      return res.status(StatusCodes.OK).json(product);
+    } catch (error) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
+    }
+  }
+}
 
 export default ProductController;
