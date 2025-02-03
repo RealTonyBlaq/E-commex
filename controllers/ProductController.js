6942f90686518ec865224ef5a31cda8c6d9840a5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-02-03 19:03:31 +0100

    Product controller

---
+++ controllers/ProductController.js
@@ -0,0 +1,33 @@
+import { isValidObjectId } from "mongoose";
+import { Product } from "../schema/product";
+import { StatusCodes } from "http-status-codes";
+
+class ProductController {
+    static async createProduct(req, res) {
+        const { name, description, price, stockQuantity, categoryId } = req.body;
+        if (!name || !description || !price || !stockQuantity || !categoryId) {
+            return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json({ error: "All fields are required" });
+        }
+        if (!isValidObjectId(categoryId)) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: "Invalid category ID" });
+        }
+        try {
+            const product = new Product({
+                name,
+                description,
+                price,
+                stockQuantity,
+                categoryId,
+            });
+            await product.save();
+            return res.status(StatusCodes.CREATED).json(product);
+        } catch (error) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
+        }
+    }
+}
+
+
+export default ProductController;
