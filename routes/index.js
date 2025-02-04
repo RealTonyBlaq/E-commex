c56ce26f0ee7a65bdae4d5f124aaeaf01acd38bd
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-02-04 22:28:33 +0100

    new routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
 import { Router } from "express";
+import ProductController from "../controllers/ProductController.js";
 import UserController from "../controllers/UserController.js";
 
 const router = Router();
 
+/* User Routes */
 router
   .route("/users")
   .post(await UserController.createUser)
   .get(await UserController.getUser)
   .put(await UserController.updateUser)
   .delete(await UserController.deleteUser);
+
+/* Product Routes */
+router
+  .route("/products")
+  .post(await ProductController.createProduct)
+  .get(await ProductController.getProduct)
+  .put(await ProductController.updateProduct)
+  .delete(await ProductController.deleteProduct);
+
 
 export default router;
