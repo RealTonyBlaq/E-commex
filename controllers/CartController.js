8d03f42e4c1b32e68b9dff262c91462fef70e25a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-02-13 19:40:57 +0100

    update

---
+++ controllers/CartController.js
@@ -0,0 +1,46 @@
+import { isValidObjectId } from "mongoose";
+import { Cart } from "../schema/cart.js";
+import { StatusCodes } from "http-status-codes";
+
+class CartController {
+  static async CreateCart(req, res) {
+    const userId = req.query.userId;
+    const { listOfProducts } = req.body;
+
+    // define methods to retrieve carted products from cache/redis
+    if (!userId)
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "No user ID passed" });
+
+    if (!isValidObjectId(userId))
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "Invalid user ID" });
+
+    if (!listOfProducts)
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "No product list passed" });
+
+    if (!Array.isArray(listOfProducts))
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "Not a list type" });
+
+    listOfProducts.forEach((product) => {
+      const { productId, quantity } = product;
+      if (productId && quantity) {
+        if (!isValidObjectId(productId))
+          return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json({ error: "Invalid product ID" });
+      }
+    }
+    try {
+        const newCart = new Cart({
+            userId,
+            items: listOfProducts,
+        });
+  }
+}
