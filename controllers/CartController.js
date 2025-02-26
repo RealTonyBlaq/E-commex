01fcf36010913aaaf249f0211942cd6f5b26b297
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-02-26 21:38:06 +0100

    added RemoveFromCart, removed CreateCart

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -5,75 +5,7 @@ import { Product } from "../schema/product.js";
 import { User } from "../schema/user.js";
 
 class CartController {
-  static async CreateCart(req, res) {
-    const userId = req.query.userId;
-    const { listOfProducts } = req.body;
-
-    // define methods to retrieve carted products from cache/redis
-    if (!userId)
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ error: "No user ID passed" });
-
-    if (!isValidObjectId(userId))
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ error: "Invalid user ID" });
-
-    if (!(await User.findById(userId)))
-      return res
-        .status(StatusCodes.NOT_FOUND)
-        .json({ error: "User not found" });
-
-    if (!listOfProducts)
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ error: "No product list passed" });
-
-    if (!Array.isArray(listOfProducts))
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ error: "Not a list type" });
-
-    if (listOfProducts.length === 0)
-      return res.status(StatusCodes.BAD_REQUEST).json({ error: "Empty list" });
-
-    const items = [];
-    for (const product of listOfProducts) {
-      const { productId, quantity } = product;
-      if (productId) {
-        if (!isValidObjectId(productId))
-          return res
-            .status(StatusCodes.BAD_REQUEST)
-            .json({ error: "Invalid product ID" });
-
-        if (!(await Product.findById(productId)))
-          return res
-            .status(StatusCodes.NOT_FOUND)
-            .json({ error: "Product not found" });
-
-        items.push({ productId, quantity });
-      }
-    }
-
-    if (items.length === 0)
-      return res.status(StatusCodes.BAD_REQUEST).json({ error: "Empty list" });
-
-    try {
-      const newCart = new Cart({
-        userId,
-        items,
-      });
-      await newCart.save();
-      return res.status(StatusCodes.CREATED).json(newCart);
-    } catch (error) {
-      return res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ error: error.message });
-    }
-  }
-
-  static async getCart(req, res) {
+  static async GetCart(req, res) {
     const cartId = req.query.cartId;
 
     if (!cartId)
@@ -87,7 +19,6 @@ class CartController {
         .json({ error: "Invalid Cart ID" });
 
     const myCart = await Cart.findById(cartId);
-
     if (!myCart)
       return res.status(StatusCodes.NOT_FOUND).json({ error: "Not found" });
     return res.status(StatusCodes.OK).json(myCart);
@@ -109,8 +40,22 @@ class CartController {
         .json({ error: "Product not found" });
 
     const items = [{ productId, quantity }];
-
     if (!cartId) {
+      if (!userId)
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ error: "No user ID passed" });
+
+      if (!isValidObjectId(userId))
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ error: "Invalid user ID" });
+
+      if (!(await User.findById(userId)))
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ error: "User not found" });
+
       try {
         const newCart = new Cart({
           userId,
@@ -133,6 +78,52 @@ class CartController {
     const myCart = await Cart.findById(cartId);
     if (!myCart)
       return res.status(StatusCodes.NOT_FOUND).json({ error: "Not found" });
+
+    myCart.items.push({ productId, quantity });
+    await myCart.save();
+    return res.status(StatusCodes.OK).json(myCart);
+  }
+
+  static async RemoveFromCart(req, res) {
+    const cartId = req.query.id;
+    const { productId, clearAll } = req.body;
+
+    if (!cartId)
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "CartId missing" });
+
+    if (!isValidObjectId(cartId))
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "Invalid Cart ID" });
+
+    const myCart = await Cart.findById(cartId);
+    if (!myCart)
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: "Cart Not found" });
+
+    if (clearAll) {
+      myCart.items = [];
+      await myCart.save();
+      return res.status(StatusCodes.OK).json(myCart);
+    }
+
+    if (productId && !isValidObjectId(productId))
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "Invalid product ID" });
+
+    const index = myCart.items.findIndex(
+      (item) => item.productId === productId
+    );
+    if (index === -1)
+      return res.status(StatusCodes.NOT_FOUND).json({ error: "Not found" });
+
+    myCart.items.splice(index, 1);
+    await myCart.save();
+    return res.status(StatusCodes.OK).json(myCart);
   }
 }
 
